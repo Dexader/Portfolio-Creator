7fb91d0e0838ec6be6e9877470e0ec9481dc2586
Evgeniy Savin <82032696+Dexader@users.noreply.github.com>
2023-01-09 23:59:20 +0700

    Testimonials block

--- script.js
+++ script.js
@@ -47,3 +47,37 @@ new Swiper('.projects__slider', {
 	},
 
 });
+
+//Slider testimonials
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide() {
+	showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+	showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName('testimonials__slide');
+
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+
+	for (let slide of slides) {
+		slide.style.display = 'none';
+	}
+	slides[slideIndex - 1].style.display = 'flex';
+}
